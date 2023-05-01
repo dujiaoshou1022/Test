301ee7914a2bd1ca1e3991a8fe1da6ee8e12fa75
dujiaoshou1022 <dujiaoshou1022@163.com>
2023-05-01 09:28:58 +0800

    Updated test1 and test2 files

--- test1.c
+++ test1.c
@@ -1,10 +1,21 @@
 /*************************************************************************
-	> File Name: test1.c
+	> File Name: test.c
 	> Author: longway.bai
 	> Mail: longway.bai@sigmastar.com.cn
-	> Created Time: 2023年05月01日 星期一 08时36分49秒
+	> Created Time: 2023年05月01日 星期一 08时26分41秒
  ************************************************************************/
 
 #include<stdio.h>
 
+
 int main()
+{
+	printf("hello world\n");
+
+	while(1)
+	{
+		printf("Nice world\n");
+	}
+	return 0;
+
+}
