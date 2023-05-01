2f65168856fddd10b73e4e4a70031c29074a5432
dujiaoshou1022 <dujiaoshou1022@163.com>
2023-05-01 08:34:56 +0800

    Added test.c file

---
+++ test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: longway.bai
+	> Mail: longway.bai@sigmastar.com.cn
+	> Created Time: 2023年05月01日 星期一 08时26分41秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main()
+{
+	printf("hello world\n");
+
+	return 0;
+}
